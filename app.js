e7a3366091d64bb515938cefd1e84eaa209b629c
Leo Vásquez Vólquez <vasquezvolquezfelixleonel@gmail.com>
2024-01-20 00:35:55 -0400

    Deleting tasks when it is required

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const messageBox = document.getElementById('messageBox'),
     tasksList = document.querySelector('.tasksList'),
     form = document.querySelector('.form'),
     errorContainer = document.createElement('DIV');
-let tweets = [];
+let tasks = [];
 
 
 loadEvents();
@@ -12,13 +12,23 @@ loadEvents();
 
 // EVENT LISTENERS
 function loadEvents() {
-    sendBtn.addEventListener('click', addTweet);
+    window.addEventListener('DOMContentLoaded', () => {
+        messageBox.focus();
+    })
+
+    sendBtn.addEventListener('click', addTask);
+
+    window.addEventListener('keydown', event => {
+        if (event.key === 'Enter') {
+            addTask(event);
+        }
+    });
 }
 
 
 
 // FUNCTIONS
-function addTweet(e) {
+function addTask(e) {
     e.preventDefault();
 
     if (messageBox.value == '') {
@@ -27,31 +37,51 @@ function addTweet(e) {
         return;
     }
 
-    createHTML(messageBox.value);
+    createHTML();
 }
 
-function createHTML(tweet) {
 
-    const tweetObj = {
+
+function createHTML() {
+
+    const taskValue = messageBox.value;
+
+    const taskObj = {
         id: Date.now(),
-        tweet
+        task: taskValue
     }
 
-    tweets = [...tweets, tweetObj]
+    tasks = [...tasks, taskObj]
+
     clearHTML();
 
-    if (tweets.length > 0) {
-        tweets.forEach(tweet => {
-            console.log(tweets)
-            const element = document.createElement('LI');
+    if (tasks.length > 0) {
+        tasks.forEach(task => {
+            console.log(tasks)
+            const element = document.createElement('LI'),
+                deleteBtn = document.createElement('A');
 
-            element.innerText = tweet.tweet;
+            deleteBtn.innerText = 'X';
+
+            deleteBtn.onclick = () => {
+                deleteTask(task.id);
+            }
+
+            element.innerText = task.task;
+            element.appendChild(deleteBtn);
             tasksList.appendChild(element);
+
+            if (task.task === '') {
+                deleteBtn.parentElement.remove();
+            }
         })
     }
 
     messageBox.value = '';
+    messageBox.focus();
 }
+
+
 
 function clearHTML() {
     while (tasksList.firstChild) {
@@ -73,12 +103,23 @@ function createError(message) {
     errorContainer.appendChild(error);
 
     setTimeout(() => {
-        errorContainer.removeChild(error);
+        error.remove();
     }, 2000);
 }
+
+
 
 function clearError() {
     while (errorContainer.firstChild) {
         errorContainer.removeChild(errorContainer.firstChild)
     }
+}
+
+
+
+function deleteTask(id) {
+    tasks = tasks.filter(task => task.id !== id);
+    console.log(tasks);
+
+    createHTML()
 }
